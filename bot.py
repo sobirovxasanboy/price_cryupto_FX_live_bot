afafe0c3bb8a423e58f1e2b0bc10a45fb2a883cd
sobirovxasanboy <Ravshanbek09163@gmail.com>
2026-06-30 01:26:38 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
-print("Bot ishlayapti")
+import os
+import requests
+from telegram import Bot
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+bot = Bot(token=BOT_TOKEN)
+
+usd = requests.get("https://cbu.uz/uz/arkhiv-kursov-valyut/json/USD/").json()[0]["Rate"]
+eur = requests.get("https://cbu.uz/uz/arkhiv-kursov-valyut/json/EUR/").json()[0]["Rate"]
+
+text = f"""📊 Valyuta kurslari
+
+🇺🇸 USD: {usd} so'm
+🇪🇺 EUR: {eur} so'm
+"""
+
+bot.send_message(chat_id=CHANNEL_ID, text=text)
